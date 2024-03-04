afc1e78837c437828133ed6c97d2c447ad051ed4
MrHuan <huanzi20100702@hotmail.com>
2024-03-04 16:57:21 +0800

    fix: 修复用户表生日字段写入空字符串的问题

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -8,28 +8,28 @@ import (
 )
 
 type User struct {
-	ID                uint64 `json:"id,omitempty" gorm:"primaryKey"`
-	Sex               int8   `json:"sex"`
-	Birthday          string `json:"birthday" gorm:"default:null"`
-	LastLoginTime     uint64 `json:"lastLoginTime" gorm:"default:null"`
-	LastLoginIp       string `json:"lastLoginIp"`
-	CreateTime        uint64 `json:"createTime" gorm:"autoUpdateTime"`
-	UpdateTime        uint64 `json:"updateTime" gorm:"autoUpdateTime"`
-	UserStatus        uint8  `json:"userStatus"`
-	UserLogin         string `json:"userLogin" gorm:"index"`
-	UserPass          string `json:"-"`
-	UserNickname      string `json:"userNickname"`
-	UserEmail         string `json:"userEmail"`
-	Avatar            string `json:"avatar"`
-	Signature         string `json:"signature"`
-	UserActivationKey string `json:"userActivationKey"`
-	Mobile            string `json:"mobile"`
-	LockTime          int64  `json:"lockTime,omitempty"`
-	ErrorSum          uint8  `json:"errorSum,omitempty"`
-	First             uint8  `json:"first,omitempty"`
-	LastEditPass      int64  `json:"lastEditPass"`
-	Openid            string `json:"openid"`
-	Super             int8   `json:"super"`
+	ID                uint64  `json:"id,omitempty" gorm:"primaryKey"`
+	Sex               int8    `json:"sex"`
+	Birthday          *string `json:"birthday" gorm:"default:null"`
+	LastLoginTime     uint64  `json:"lastLoginTime" gorm:"default:null"`
+	LastLoginIp       string  `json:"lastLoginIp"`
+	CreateTime        uint64  `json:"createTime" gorm:"autoUpdateTime"`
+	UpdateTime        uint64  `json:"updateTime" gorm:"autoUpdateTime"`
+	UserStatus        uint8   `json:"userStatus"`
+	UserLogin         string  `json:"userLogin" gorm:"index"`
+	UserPass          string  `json:"-"`
+	UserNickname      string  `json:"userNickname"`
+	UserEmail         string  `json:"userEmail"`
+	Avatar            string  `json:"avatar"`
+	Signature         string  `json:"signature"`
+	UserActivationKey string  `json:"userActivationKey"`
+	Mobile            string  `json:"mobile"`
+	LockTime          int64   `json:"lockTime,omitempty"`
+	ErrorSum          uint8   `json:"errorSum,omitempty"`
+	First             uint8   `json:"first,omitempty"`
+	LastEditPass      int64   `json:"lastEditPass"`
+	Openid            string  `json:"openid"`
+	Super             int8    `json:"super"`
 }
 
 func (receiver *User) TableName() string {
@@ -38,8 +38,11 @@ func (receiver *User) TableName() string {
 
 func (receiver *User) AfterFind(*gorm.DB) (err error) {
 	// 转换日期格式
-	if receiver.Birthday != "" {
-		receiver.Birthday, _ = time_tool.ChangeFormat(time.RFC3339, time.DateOnly, receiver.Birthday)
+	if receiver.Birthday != nil {
+		birthday, _ := time_tool.ChangeFormat(time.RFC3339, time.DateOnly, *receiver.Birthday)
+		receiver.Birthday = &birthday
+	} else {
+		receiver.Birthday = nil
 	}
 	return
 }
